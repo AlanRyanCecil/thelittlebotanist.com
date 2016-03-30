d8d97ead9e1e595402e34e7dc2776573eacd349e
Alan Cecil <alan.cecil@gmail.com>
2016-03-30 13:56:59 -0700

    the little botanist

--- controllers/controllers.js
+++ controllers/controllers.js
@@ -1,5 +1,5 @@
 angular.module('ThelittlebotanistApp')
 
     .controller('HomeController', ['$scope', function ($scope) {
-        $scope.greeting = "Welcome to The Little Botanist.";
+        $scope.greeting = "The Little Botanist.";
     }])
